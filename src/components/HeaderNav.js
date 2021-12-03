514fdc86a1dbc63861bf369d5a12a17c5a152050
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2021-12-03 21:10:23 +0100

    structure

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderNav = () => {
+  return (
+    <div className="header-nav-box">
+      <div className="box-one"></div>
+      <div className="box-two"></div>
+    </div>
+  );
+};
+
+export default HeaderNav;
